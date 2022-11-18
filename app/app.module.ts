848d1f3b2d72d97ac806668bbe0ad016ddc17b0c
MarieWillersinn <107075535+MarieWillersinn@users.noreply.github.com>
2022-11-18 19:04:48 +0100

    Firebase

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
     AppRoutingModule
   ],
--- environments/environment.ts
+++ environments/environment.ts
@@ -3,8 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+    firebase: {
+      apiKey: "AIzaSyC12aCAV0INTCpAS6cH5r-k9VUDP26iWxk",
+      authDomain: "ring-of-fire-24017.firebaseapp.com",
+      projectId: "ring-of-fire-24017",
+      storageBucket: "ring-of-fire-24017.appspot.com",
+      messagingSenderId: "81081871535",
+      appId: "1:81081871535:web:e05a8deb52dcffff4b385a"
+    }
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
